46a608b8ffccc7f7f2b57eae6b1728f03091312d
glalaj <gentjana.lalaj@magoware.tv>
2017-11-23 16:55:07 +0100

    Updated device brand length

--- modules/mago/server/models/devices.js
+++ modules/mago/server/models/devices.js
@@ -61,7 +61,7 @@ module.exports = function(sequelize, DataTypes) {
             allowNull: false
         },
         device_brand: {
-            type: DataTypes.STRING(45),
+            type: DataTypes.STRING(100),
             allowNull: true
         },
         os: {
